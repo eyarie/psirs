684e9042f93354bf62427e2ee120f43f90885474
Tony <you@example.com>
2017-08-05 14:59:35 +0100

    router.js file updated with two new files

--- controllers/router.js
+++ controllers/router.js
@@ -1,9 +1,9 @@
 var individual = require('./individual.js'),
-var individual = require('./corporate.js')
+    corporate = require('./corporate.js');
 
 function route(app){
 
-individuals.route(app);
+individual.route(app);
 corporate.route(app);
 
 }
